0fc88a0fe66d3d601f678cf79c4bdb9808f5aef2
boyarkinev <60974381+boyarkinev@users.noreply.github.com>
2020-10-02 01:29:32 +0300

    Updates

---
+++ precache-manifest.b6bdf0f8c12cb8ca5c90f42f302045f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c891bbecebbc140337cbbf6a58477f",
+    "url": "/country-searcher/index.html"
+  },
+  {
+    "revision": "200ac7bdfded17240165",
+    "url": "/country-searcher/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "66ac8985bd18d46f2e89",
+    "url": "/country-searcher/static/css/main.6ae9e650.chunk.css"
+  },
+  {
+    "revision": "200ac7bdfded17240165",
+    "url": "/country-searcher/static/js/2.8eafbbc2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/country-searcher/static/js/2.8eafbbc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ac8985bd18d46f2e89",
+    "url": "/country-searcher/static/js/main.b95c1363.chunk.js"
+  },
+  {
+    "revision": "5d1bc1c1ccca6bd59d00",
+    "url": "/country-searcher/static/js/runtime-main.23d15ea6.js"
+  }
+]);
